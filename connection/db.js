e7f9eeb25ccaf9b5a82c690b061b0f7006de8cc9
Jody-septiawan <jody.septiawan5@gmail.com>
2022-02-09 01:49:43 +0900

    update

--- connection/db.js
+++ connection/db.js
@@ -1,6 +1,10 @@
 const { Pool } = require('pg');
 
-const connectionString = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
+let connectionString = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
+
+connectionString = process.env.DATABASE_URL
+  ? process.env.DATABASE_URL
+  : connectionString;
 
 const pool = new Pool({
   connectionString: connectionString,
